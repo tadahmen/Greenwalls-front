4640c67d45f6315f2594985bd9d8e1d23cee9587
tadahmen <tadahmen@gmail.com>
2016-04-09 13:38:36 +0200

    did some small refactoring

--- src/PlantMenu.js
+++ src/PlantMenu.js
@@ -22,14 +22,48 @@ class PlantMenu extends React.Component {
     })
   }
 
-  pastePicture(event) {
+  savePlantInSpot(plantSpotId, plantId, plantsContainerId){
+    let newPlant = {     //to ensure the new data are used. (data in this.state could still be the 'old' data)
+      // plant_id: plantId,
+      plants_container_id: 4
+    };
+    console.log("SAVING NEW PLANT IN SPOT");
+    console.log("id (of plantspot):" + plantSpotId);
+    console.log("plant_id :" + plantId);
+    console.log("plants_container_id :" + plantsContainerId);  //to check if the new values were taken
+
+    jQuery.ajax({
+      type: "PUT",
+      url:`http://localhost:5000/plants_containers/${plantsContainerId}/plant_spots/${plantSpotId}.json`,
+      data: JSON.stringify({
+        plant_spot: {plants_container_id: 10}
+      }),
+      contentType: "application/json",
+      dataType: "json"
+    })
+      .done(function(data) {
+        console.log(data);
+        })
+      .fail(function(error) {
+        console.log(error);
+      });
+  }
+
+  putPictureInSpot(plantSpotPosition, plantPicture) {
+    document.getElementsByClassName('plantSpot')[plantSpotPosition].setAttribute("src", plantPicture);
+  }
+
+  putPlantInSpot(event) {
     // event.preventDefault();
-    document.getElementsByClassName('plantSpot')[event.plantSpot].setAttribute("src", event.picture);
+    console.log("put picture:" + event.plantPicture + " in spot with position: " + event.plantSpotPosition + "and id: " + event.plantSpotId);
+    this.putPictureInSpot(event.plantSpotPosition, event.plantPicture);
+    // document.getElementsByClassName('plantSpot')[event.plantSpot].setAttribute("src", event.plantPicture);
+    // this.savePlantInSpot(event.plantSpotId, event.plantId, event.plantsContainerId)
   }
 
   componentDidMount() {
+    console.log("mount PlantMenu");
     this.reloadPlants();
-    console.log("PlantMenu did mount");
   }
 
   render(){
@@ -40,7 +74,7 @@ class PlantMenu extends React.Component {
             {
               this.state.plants.map(function(plant, i){
                 return(<li>
-                  <button onClick={this.pastePicture.bind(this, {picture: plant.picture, plantSpot: this.props.plantSpot})}>
+                  <button onClick={this.putPlantInSpot.bind(this, {plantPicture: plant.picture, plantSpotPosition: this.props.plantSpotPosition, plantSpotId: this.props.plantSpotId, plantId: plant.id, plantsContainerId: this.props.plantsContainerId} )}>
                     {plant.name}
                   </button></li>);
             }, this)}
