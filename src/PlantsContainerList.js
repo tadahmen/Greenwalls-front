4374294a695a3aae0c562d8ec8a9770ae410d040
tadahmen <tadahmen@gmail.com>
2016-04-14 19:05:43 +0200

    functionalities for easier use, and some basic markup changes

--- src/PlantsContainerList.js
+++ src/PlantsContainerList.js
@@ -13,37 +13,37 @@ class PlantsContainerList extends React.Component {
       plantsContainerName: "",
       count: 0,
       plants: [],
-      timesLoaded: 0
+      timesLoaded: 0,
     }
   }
 
-  reloadPlantsContainers(event) {
+  reloadPlantsContainers(event) {   //loads (new) list of plantscontainers from db
     let component = this;
-    console.log("LOADING PLANTCONTAINERS FROM DB");
-    console.log("containerid after reload: " + component.state.plantsContainerId)
+    console.log("LOADING CONTAINERLIST FROM DB");
+    console.log("container id after (re)loading: " + component.state.plantsContainerId)
 
-    jQuery.getJSON(`http://localhost:5000/plants_containers`, function(data) {
-      console.log("loaded containerlist: " + data);
+    jQuery.getJSON(`http://localhost:5000/plants_containers`, function(data) {  //request to db.
+      console.log("CONTAINERLIST LOADED: " + data);
       component.setState({
         plantsContainers: data.plants_containers,
         count: data.meta.count
       })
     })
-    .done(function() {
-      console.log(component.state.newContainerCreated);
-      if (component.state.newContainerCreated) { //true if a new container just has been created
+    .done(function() {  //if new container was created, automatically select it to fill it with 1 plantspot and show it
+      console.log(">>>>>CHECK new container created? " + component.state.newContainerCreated);
+      if (component.state.newContainerCreated) { //TRUE if new container has just been created
         let containersTotal = component.state.plantsContainers.length;
-        let lastContainer = component.state.plantsContainers[containersTotal-1];//the total-1 gives the position of the current last container
-        console.log(">>>>>CHECK VALUE: " + containersTotal);
+        let newContainer = component.state.plantsContainers[containersTotal-1];//the total-1 gives the position of the current last container
+        console.log("total nr. of containers: " + containersTotal);
         component.setState({
-          plantsContainerId: lastContainer.id, //to directly show 'add plantspot' button for new container
-          newContainerCreated: false  //sets this value back to false
+          plantsContainerId: newContainer.id, //selects container. And PlantSpots' componentWillReceiveProps is called, fills it with 1 plantspot and displays it
+           newContainerCreated: false
         });
       }
     });
   }
 
-  loadPlants() {
+  loadPlants() {  //loads all plants from db. Done here, to easily reach this data when needed in plantSpots (a child)
     let component = this;
     console.log("LOADING PLANTLIST FROM DB");
 
@@ -55,14 +55,25 @@ class PlantsContainerList extends React.Component {
     })
   }
 
-  createPlantsContainer(event) {
+  // toggleNewContainerSwitch() {
+  //   this.setState({
+  //     newContainerCreated: !this.state.newContainerCreated   //a switch to make reloadPlantsContainers select the new container, so it's (empty) content is displayed on screen
+  //   });
+  // }
+
+  createPlantsContainer(event) {  //to make new, empty plants container in db
     let component = this;
-    let newPlantsContainer ={
+    let newPlantsContainer = { //the Container.
       id: null,
       name: "new plants-container"
     };
 
-    jQuery.ajax({
+    // component.toggleNewContainerSwitch();  //sets newContainerCreated to TRUE, so reloadPlantsContainers will select the new container
+    this.setState({
+      newContainerCreated: true   //a switch to make reloadPlantsContainers select the new container, so it's (empty) content is displayed on screen
+    });
+
+    jQuery.ajax({   //post request to db
       type:'POST',
       url: `http://localhost:5000/plants_containers`,
       data: JSON.stringify({
@@ -71,13 +82,10 @@ class PlantsContainerList extends React.Component {
       contentType: "application/json",
       dataType: "json"
     })
-    .done(function() {
+    .done(function() { //to directly show new container in list on screen
       console.log("new plants container succesfully created");
-      component.setState({
-        newContainerCreated: true   //a switch to make reloadPlantsContainers select the new container to appear on screen
-      });
-      component.reloadPlantsContainers(function() {
-      }); //to directly show new container in list on screen
+
+      component.reloadPlantsContainers() //shows new container on screen
     })
   }
 
@@ -94,11 +102,11 @@ class PlantsContainerList extends React.Component {
     .done( function(){
       console.log("container succesfully deleted");
       console.log("CHECK id of deleted container: " + event);
-      component.reloadPlantsContainers();   //plantcontainerlist on screen refreshes
-      if (component.state.plantsContainerId === event) {
-        component.setState(
-            {plantsContainerId: ""} //if plantspots shown are that of the deleted container, then screen refreshes without them
-        );
+      component.reloadPlantsContainers();     //plantcontainerlist on screen refreshes
+      if (component.state.plantsContainerId === event) { //if plantspots shown are that of the deleted container...
+        component.setState({
+            plantsContainerId: ""   // ...the screen refreshes without any plantspots.
+        });
       }
     })
   }
@@ -110,18 +118,23 @@ class PlantsContainerList extends React.Component {
   }
 
   renderPlantSpots(plantsContainerId, plantsContainerName){
-    return<PlantSpots className="placeHolder" onChange={this.reloadPlantsContainers.bind(this)} plantsContainerId={plantsContainerId} plantsContainerName={plantsContainerName} plants={this.state.plants}/>;
+    return<PlantSpots className="placeHolder"
+        onChange={this.reloadPlantsContainers.bind(this)}
+        plantsContainerId={plantsContainerId}
+        plantsContainerName={plantsContainerName}
+        plants={this.state.plants}
+        containerIsNew={this.state.newContainerCreated}
+    />;
   }
 
   setContainerId(event) {
     console.log("SELECTING CONTAINER");
     console.log("new plantsContainerId:" + event.id);
-    //let newState =
     this.setState({
       plantsContainerId: event.id,
       plantsContainerName: event.name
     })
-      console.log("this.state.plantsContainerId after setState is:" + component.state.plantsContainerId)
+      console.log("this.state.plantsContainerId after setState is old id:" + this.state.plantsContainerId)
   }
 
   render(){
@@ -129,27 +142,27 @@ class PlantsContainerList extends React.Component {
       <div className="plantscontainer-list">
         <p>Plantcontainers:</p>
         {
-          this.state.plantsContainers.map(function(plantsContainer, i){
+          this.state.plantsContainers.map(function(plantsContainer, i){ //shows all plantscontainernames, each with delete button
 
             return(
               <span className="wrapper">
-                <button className="plantsContainer" onClick={this.setContainerId.bind(this, plantsContainer)}>
+                <button className="plantsContainer" onClick={this.setContainerId.bind(this, plantsContainer)}> {/*clicked container is selected*/}
                   <span className="containerName"> {plantsContainer.name} </span>
                 </button>
                 <button className="deleteContainer" onClick={this.deletePlantsContainer.bind(this, plantsContainer.id)}>
-                  <span className="deleteSymbol"> x </span>
+                  <span className="deleteSymbol"> x </span> {/*container is deleted on clicking 'x'*/}
                 </button>
               </span>
             );
           }, this)
 
         }
-        <button className="add-container" onClick={this.createPlantsContainer.bind(this)}>
+        <button className="add-container" onClick={this.createPlantsContainer.bind(this)}> {/*creates new container on click*/}
           <span className="addContainer-symbol"> + </span>
         </button>
         <div>
           {
-            this.state.plantsContainerId !== ""
+            this.state.plantsContainerId !== "" //to see that on starting the app, no unnecessary rendering is done
               ? this.renderPlantSpots(this.state.plantsContainerId, this.state.plantsContainerName)
               : <p>Choose a plantcontainer</p>
           }
